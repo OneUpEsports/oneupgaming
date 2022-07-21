c966ad18cdbbb114ed67b46b91c73bc3759c28a1
Konrad Koni <eventbeate@gmail.com>
2022-07-21 18:21:23 +0200

    fixed wrong directory

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import LandingPage from './components/LandingPage/LandingPage'
+import LandingPage from '../components/LandingPage/LandingPage'
 
 const Home: NextPage = () => {
   return (
