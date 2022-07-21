3460a07c1b1f982e5c92041eb3ae2c764073f449
Konrad Koni <eventbeate@gmail.com>
2022-07-21 21:40:16 +0200

    centered div :poop:

--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -3,9 +3,11 @@ import React from "react";
 function Landing() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline text-center">
-                Hello there!
+        <div className="h-screen w-full bg-black text-center grid place-content-center">
+            <h1 className="text-3xl font-bold text-center text-white">
+                oneupgaming
             </h1>
+        </div>
         </>
     )
 }
