6e37c8c5ad33762bf81843b7a87eebf1f5791766
Konrad Koni <eventbeate@gmail.com>
2022-07-21 18:12:33 +0200

    moved to components

---
+++ pages/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Landing() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline text-center">
+                Hello there!
+            </h1>
+        </>
+    )
+}
+
+export default Landing;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import LandingPage from './components/LandingPage/LandingPage'
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <LandingPage />
   )
 }
 
